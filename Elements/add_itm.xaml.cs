26638e607d5ad5dcf1835d7954826f0d8283179f
TeHe4K <hryblackwii@gmail.com>
2026-02-25 22:52:00 +0500

    Разработано окно Gift_main. Коневский

---
+++ Elements/add_itm.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PR23_Konevskii.Elements
+{
+    /// <summary>
+    /// Логика взаимодействия для add_itm.xaml
+    /// </summary>
+    public partial class add_itm : UserControl
+    {
+        public add_itm()
+        {
+            InitializeComponent();
+        }
+    }
+}
