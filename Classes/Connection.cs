58af5815637504bf67b20ad66ac17493226818d0
TeHe4K <hryblackwii@gmail.com>
2026-02-25 23:19:44 +0500

    Разработан метод Connection. Коневский

---
+++ Classes/Connection.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.IO;
+using System.Linq;
+using System.Security.RightsManagement;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR23_Konevskii.Classes
+{
+    public class Connection
+    {
+        public List<Gift> gifts = new List<Gift>();
+
+        public enum tabels
+        {
+            gifts
+        }
+        public string localPath = "";
+
+        public OleDbDataReader QueryAccess(string query)
+        {
+            try
+            {
+                localPath = Directory.GetCurrentDirectory();
+                OleDbConnection connect = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + localPath + "/accesbase.accdb");
+                connect.Open();
+                OleDbCommand cmd = new OleDbCommand(query, connect);
+                OleDbDataReader reader = cmd.ExecuteReader();
+                return reader;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+        public void LoadData (tabels zap)
+        {
+            try
+            {
+                OleDbDataReader itemQuery = QueryAccess("SELECT * FROM [" + zap.ToString() + "] ORDER BY [Код]");
+                gifts.Clear();
+                while (itemQuery.Read())
+                {
+                    Gift newE1 = new Gift();
+                    newE1.id = Convert.ToInt32(itemQuery.GetValue(0));
+                    newE1.name = Convert.ToString(itemQuery.GetValue(1));
+                    newE1.description = Convert.ToString(itemQuery.GetValue(2));
+                    newE1.date = Convert.ToString(itemQuery.GetValue(3));
+                    newE1.adress = Convert.ToString(itemQuery.GetValue(4));
+                    newE1.mail = Convert.ToString(itemQuery.GetValue(5));
+                }
+            }
+            catch
+            {
+                Console.WriteLine("NULL");
+            }
+        }
+    }
+}
---
+++ Classes/Gift.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR23_Konevskii.Classes
+{
+    public class Gift
+    {
+        public int id {  get; set; }
+        public string name { get; set; }
+        public string adress { get; set; }
+        public string mail { get; set; }
+        public string date { get; set; }
+        public string description { get; set; }
+    }
+}
